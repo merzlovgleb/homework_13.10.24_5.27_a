f432c0a0479a13d58ea8c0afd25b894fda9fd0a1
merzlovgleb <merzlovgleb@gmail.com>
2024-10-19 14:31:32 +0300

    5.27_a

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int c = 100; c <= 500; c++) {
+            sum = sum + c;
+        }
+            System.out.println(sum);
+        }
+    }
